8b7fbbd9fd565f7618ad5ffd63f3916cf45b72a6
Farshadrushdi <farshadrushdi20@gmail.com>
2024-03-18 10:07:03 +0530

    mini project

---
+++ leave/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import student,teacher,leave
+
+# Register your models here.
+admin.site.register(student)
+admin.site.register(teacher)
+admin.site.register(leave)
